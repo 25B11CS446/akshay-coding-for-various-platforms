e76f60b78fbc72fc45a5e18a9ce51280bda809a5
25B11CS446 <akshayjanardhan1@gmail.com>
2025-12-06 09:20:01 +0530

    Add files via upload

---
+++ without arguments and no return values.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void add()
+{
+	int a,b,c;
+	printf("Enter the values of a,b:");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("sum is %d\n",c);
+}
+int main()
+{
+	 add();
+	 add();
+	 add();
+	return 0;
+}
