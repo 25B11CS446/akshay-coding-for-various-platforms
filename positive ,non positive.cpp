5245d0f464cacb1a17ecb1ff43dd201dc1b7f165
25B11CS446 <akshayjanardhan1@gmail.com>
2025-12-08 12:36:24 +0530

    Add files via upload

---
+++ positive ,non positive.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int num=20;
+	if(num>0)
+	{
+		printf("num is positive.\n");
+		if(num %2== 0)
+		{
+			printf("num is even \n");
+	    }
+	    else
+		{
+		printf("num is odd\n");
+	    }
+     }
+     else
+     {
+     printf("num is non positive.\n");
+     }
+     return 0;
+ }
+	 
