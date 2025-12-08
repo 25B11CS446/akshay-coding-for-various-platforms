53f47d738236adc3dafb9bd584498ce1f786e7c1
25B11CS446 <akshayjanardhan1@gmail.com>
2025-12-08 12:26:22 +0530

    Add files via upload

---
+++ greter than 5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num=10;
+	if(num>5)
+	{
+		printf("the number is greater than 5\n");
+    }
+     printf("the message always prints");
+     return 0;
+ }
---
+++ hello world multiple.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void display()
+{
+	printf("hello world \n");
+
+}
+int main()
+{
+	display();
+	display();
+	return 0;
+}
