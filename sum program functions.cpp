b1e13e0ba5781a004760dfb38be94a0c01014676
25B11CS446 <akshayjanardhan1@gmail.com>
2025-12-08 12:29:21 +0530

    Add files via upload

---
+++ sum program functions.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void add()
+{
+	int a,b,sum=0;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("sum is:%d\n",sum);
+}
+int main()
+{
+	add();
+	add();
+	add();
+	return 0;
+}
